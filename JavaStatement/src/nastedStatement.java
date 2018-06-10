18da7e369084d4d7b47023ada647ee6ef89623ea
HP <HP@DESKTOP-KG1U8R5>
2018-06-10 15:13:17 +0530

    this is the example of nasted statement in JAVA

---
+++ JavaStatement/src/nastedStatement.java
@@ -0,0 +1,33 @@
+
+public class nastedStatement {
+	public static void main(String[] args) {
+		 int a=1000;
+		 int b=200;
+		 int c=3000;
+
+	if(a>b) {
+	                       	if(a>c) {
+			                         System.out.println("A is greater no");
+		                            }else {
+			                                System.out.println("C is greater no");
+		                                   }
+        	}else
+        		
+        			   
+        	        if(b>c) {
+        	            	System.out.println("B is greater no");
+        	                }else {
+        		                  System.out.println("C is greater no");
+                                	}
+        	
+        		
+        	
+           
+	
+	
+	
+	
+	}
+	
+}
+

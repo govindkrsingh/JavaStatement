2c0019dd88f7cff244ac5af5501c6cfe6a328eb4
govind singh <govindsingh9771@gmail.com>
2018-06-10 19:24:07 +0530

    this is example of switch statements

---
+++ JavaStatement/src/SwitchStatement.java
@@ -0,0 +1,26 @@
+
+public class SwitchStatement {
+	public static void main(String[] args) {
+		
+		int a=10;
+		
+		switch(a) {
+		
+		case 10:
+			System.out.println("select 10");
+			break;
+		case 20:
+			System.out.println("select 20");
+			break;
+		case 30:
+			System.out.println("select 30");
+			break;
+		default:
+		 System.out.println("not 10,20,30");
+				
+		}
+		
+		
+	}
+
+}
